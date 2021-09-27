6e0d7f3c7b3fe3d082bb7a81a00e62f2c5a4d0b9
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2021-09-27 17:51:09 -0400

    main functions are completed

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function LightSwitchButton() {
+
+  const [light, setLight] = useState("off");
+
+  const handleClick = () => {
+    setLight(light === "on" ? "off" : "on");
+  }
+
+  return (
+    <button 
+    className="LightSwitchButton"
+    onClick={handleClick}
+    >
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    </button>
+  )
+}
+
+export default LightSwitchButton;
