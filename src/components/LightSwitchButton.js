59df185337eb20264a1457543cf690a6ed88de45
kevinzhu2019 <kevinzhu.zyk@gmail.com>
2021-09-27 18:38:46 -0400

    move some state hooks from component to App.js

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,17 +1,11 @@
-import { useState } from "react";
+function LightSwitchButton(props) {
 
-function LightSwitchButton() {
-
-  const [light, setLight] = useState("off");
-
-  const handleClick = () => {
-    setLight(light === "on" ? "off" : "on");
-  }
+  const {light, switchLight} = props;//props is a object, so use object desctructure
 
   return (
     <button 
     className="LightSwitchButton"
-    onClick={handleClick}
+    onClick={switchLight}
     >
       {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
